a0ef99a84ec0854316e8bb319556244f0a267ece
MitchGirard <MitchGirard@lnwconsulting.com>
2022-09-12 04:24:46 -0400

    Rename Project

--- NavigatorTemplate/Properties/Settings.Designer.cs
+++ NavigatorTemplate/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NavigatorTemplate.Properties {
+namespace PathLenghtCrawl.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
