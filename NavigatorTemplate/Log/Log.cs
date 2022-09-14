80a01e232753f610eac187e079da7c2fb254c676
MitchGirard <MitchGirard@lnwconsulting.com>
2022-09-14 19:04:22 -0400

    Log file and folder

---
+++ NavigatorTemplate/Log/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathLenghtCrawl.Log
+{
+    public class Log
+    {
+
+        public static void WriteLog(string path, string value)
+        {
+            using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(path + System.IO.Path.DirectorySeparatorChar + "log.log", true, Encoding.UTF8))
+            {
+                logFile.WriteLine(value);
+            }
+        }
+    }
+}
