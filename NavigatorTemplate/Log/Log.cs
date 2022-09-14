113c7fa99176a15529545c718744cc4ecdab2d61
MitchGirard <MitchGirard@lnwconsulting.com>
2022-09-14 19:06:44 -0400

    Log Stuff

--- NavigatorTemplate/Log/Log.cs
+++ NavigatorTemplate/Log/Log.cs
@@ -4,9 +4,8 @@ using System.Text;
 
 namespace PathLenghtCrawl.Log
 {
-    public class Log
+    public static class Log
     {
-
         public static void WriteLog(string path, string value)
         {
             using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(path + System.IO.Path.DirectorySeparatorChar + "log.log", true, Encoding.UTF8))
